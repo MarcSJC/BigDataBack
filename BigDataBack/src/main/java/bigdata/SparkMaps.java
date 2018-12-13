32ea747c9d4b98f8b88c7b827298e3126281c48a
Marc Saint-Jean-Clergeau <saintjeanclergeau.marc@gmail.com>
2018-12-13 14:46:20 +0100

    Test...

--- BigDataBack/src/main/java/bigdata/SparkMaps.java
+++ BigDataBack/src/main/java/bigdata/SparkMaps.java
@@ -2,32 +2,27 @@ package bigdata;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.input.PortableDataStream;
 
 import scala.Tuple2;
 
 public class SparkMaps {
+	
+	final static int dem3Size= 1201;
 
 	public static void main(String[] args) {
-		
-		SparkConf conf = new SparkConf().setAppName("TP Spark");
+		SparkConf conf = new SparkConf().setAppName("SparkMaps");
 		JavaSparkContext context = new JavaSparkContext(conf);
-		JavaRDD<String> rdd;
-		rdd = context.textFile(args[0]);
-		System.out.println(">>>>>>>>>>>>>>>>>>>> Old Nb Repartitions : " + rdd.getNumPartitions());
+		int data[][] = new int[dem3Size][dem3Size];
+		JavaPairRDD<String, PortableDataStream> rdd;
+		rdd = context.binaryFiles(args[0]);
 		rdd = rdd.repartition(4);
-		System.out.println(">>>>>>>>>>>>>>>>>>>> New Nb Repartitions : " + rdd.getNumPartitions());
-		rdd = rdd.filter((line) -> {
-			String tab[] = line.split(",");
-			return (!tab[4].isEmpty() && tab[4] != "" && tab[4].matches("\\d+"));
-		});
-		JavaRDD<Tuple2<String, Integer>> rdd2 = rdd.map((line) -> {
-			String tab[] = line.split(",");
-			return new Tuple2<String, Integer>(tab[2], Integer.valueOf(tab[4]));
-		});
-		rdd2.repartition(4);
-		System.out.println(">>>>>>>>>>>>>>>>>>> count : " + rdd2.count());
+		System.out.println(">>>>>>>>>>>>>>>>>>> count : " + rdd.count());
+		System.out.println(">>>>>>>>>>>>>>>>>>> example id : " + rdd.id());
+		System.out.println(">>>>>>>>>>>>>>>>>>> example name : " + rdd.name());
 	}	
 
 	
