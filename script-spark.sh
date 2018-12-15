9c231da0596e24efca0c95f1c1c07e2fca1fa3be
Marc Saint-Jean-Clergeau <saintjeanclergeau.marc@gmail.com>
2018-12-15 19:49:15 +0100

    Changement de fichier test

--- script-spark.sh
+++ script-spark.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-spark-submit --executor-memory 512M --num-executors 4 --executor-cores 2 --class bigdata.SparkMaps $HOME/git/BigDataBack/BigDataBack/target/TPSpark-0.0.1.jar /user/raw_data/dem3/N00E006.hgt
+spark-submit --executor-memory 512M --num-executors 4 --executor-cores 2 --class bigdata.SparkMaps $HOME/git/BigDataBack/BigDataBack/target/TPSpark-0.0.1.jar /user/raw_data/dem3/N43W001.hgt
