e502bbd0034f933f0d33e7083b5f4a72ce1f4ad4
Marc Saint-Jean-Clergeau <saintjeanclergeau.marc@gmail.com>
2018-12-16 18:15:58 +0100

    Test

--- BigDataBack/src/main/java/bigdata/SparkMaps.java
+++ BigDataBack/src/main/java/bigdata/SparkMaps.java
@@ -63,7 +63,7 @@ public class SparkMaps {
 			minh = Integer.min(value, minh);
 			data[i][j] = value;*/
 			int value = 0;
-			value = (b[1] << 8) | b[0];
+			value = b[0] + (b[1] * 256);
 			if (value < 0) value += 256;
 			value = Integer.min(value, maxh);
 			//value = Integer.max(value, minh);
